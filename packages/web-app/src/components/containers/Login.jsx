a7f44690a75bbcacb74e784245b5a0a796f06361
Anatol Hrabouski <anatol.hrabouski@allegro.com>
2024-10-16 15:02:55 +0200

    add login page

---
+++ packages/web-app/src/components/containers/Login.jsx
@@ -0,0 +1,36 @@
+import { GoogleLogin } from '@react-oauth/google';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+
+export default function Login() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const usr = JSON.parse(localStorage.getItem('user'));
+    setUser(usr);
+  }, [])
+  return (
+    <div>
+      <GoogleLogin
+        onSuccess={credentialResponse => {
+          const decoded = JSON.parse(atob(credentialResponse.credential.split('.')[1]));
+          localStorage.setItem('auth', JSON.stringify(credentialResponse))
+          localStorage.setItem('user', JSON.stringify(decoded))
+          setUser(decoded);
+          console.log("Logged in", credentialResponse, decoded);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
+      <div>
+        {'Logged in as: ' + user?.email}
+      </div>
+      <button onClick={() => {
+        localStorage.removeItem('auth');
+        localStorage.removeItem('user');
+        setUser(null);
+      }}>Logout</button>
+    </div>
+  )
+}
--- packages/web-app/src/components/containers/index.js
+++ packages/web-app/src/components/containers/index.js
@@ -1,2 +1,3 @@
 export { default as GraphEditor } from './GraphEditor'
 export { default as User } from './User'
+export { default as Login } from './Login'
